532208d7d934b1e143a0050994c87d5c0b542556
unknown <alexboyuchen@gmail.com>
2020-08-20 19:16:41 -0700

    organized codes and added projectiles

--- main.py
+++ main.py
@@ -1,52 +1,27 @@
 import time
 
 import pygame
-
+from player import player
 pygame.init()
+
 winwidth = 1000
 winheight = 700
 window = pygame.display.set_mode((winwidth,winheight))
 pygame.display.set_caption("Hello World")
 run = True
 gameclock = pygame.time.Clock()
-xcoord = 50
-ycoord = 50
-velocity  = 5
-height = 100
-width = 100
-rotorcount = 0
-
-mainplane = pygame.image.load("images/Aircraft_08.png")
-mainplaneshadow = pygame.image.load("images/Aircraft_08_shadow.png")
-rotor = pygame.image.load("images/rotor01.png")
 def redraw():
-    global rotorcount
     window.fill((135,206,235))
-    window.blit(mainplaneshadow,(xcoord-5,ycoord-5))
-    window.blit(mainplane,(xcoord,ycoord))
-    if rotorcount <= 4 :
-        window.blit(rotor,(xcoord+5,ycoord+13))
-        window.blit(rotor,(xcoord+47,ycoord+13))
-        
-    elif rotorcount >= 5:
-        rotorcount = 0 
-    rotorcount+=1
+    mainchar.move(keys,window)
     pygame.display.update()
+mainchar = player(500,350,100,100)
 while run:
     gameclock.tick(60) # fps controller
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_w] and ycoord >= velocity:
-         ycoord -= velocity
-    if keys[pygame.K_s] and ycoord <= winheight-(height+velocity):
-        ycoord += velocity
-    if keys[pygame.K_a] and xcoord >= velocity:
-        xcoord -= velocity
-    if keys[pygame.K_d] and xcoord <= winwidth-(width+velocity):
-        xcoord += velocity
+  
     redraw()
     
 pygame.quit()
---
+++ player.py
@@ -0,0 +1,50 @@
+import pygame
+from projectiles import bullet
+class player():
+    def __init__(self,xcoord,ycoord,width,height):
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        self.velocity = 5
+        self.height = height
+        self.width = width
+        self.rotorcount = 0
+        self.mainplane = pygame.image.load("images/Aircraft_08.png")
+        self.mainplaneshadow = pygame.image.load("images/Aircraft_08_shadow.png")
+        self.rotor = pygame.image.load("images/rotor01.png")
+        self.bullets = []
+        self.bulletdelay = 0
+    def move(self,keys,window):
+        winwidth,winheight = window.get_size()
+        if keys[pygame.K_w] and self.ycoord >= self.velocity:
+         self.ycoord -= self.velocity
+        if keys[pygame.K_s] and self.ycoord <= winheight-(self.height+self.velocity):
+            self.ycoord += self.velocity
+        if keys[pygame.K_a] and self.xcoord >= self.velocity:
+            self.xcoord -= self.velocity
+        if keys[pygame.K_d] and self.xcoord <= winwidth-(self.width+self.velocity):
+            self.xcoord += self.velocity
+        if keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]:
+             b = bullet(self.xcoord+20,self.ycoord,window)      #old value : 0
+             b2 = bullet(self.xcoord+62,self.ycoord,window)  #old value : +82
+             if len(self.bullets)<98 and self.bulletdelay <= 0:
+                self.bullets.append(b)
+                self.bullets.append(b2)
+                self.bulletdelay = 5
+            
+        for b in self.bullets:
+            window.blit(b.image,(b.x,b.y))
+            if not b.move():
+                self.bullets.pop(self.bullets.index(b))
+        window.blit(self.mainplaneshadow,(self.xcoord-5,self.ycoord-5))
+        window.blit(self.mainplane,(self.xcoord,self.ycoord))
+        if self.rotorcount <= 4 :
+            window.blit(self.rotor,(self.xcoord+5,self.ycoord+13))
+            window.blit(self.rotor,(self.xcoord+47,self.ycoord+13))
+        elif self.rotorcount >= 5:
+            self.rotorcount = 0 
+        
+        self.rotorcount+=1
+
+        if self.bulletdelay >= 0:
+            self.bulletdelay-=1
+        
