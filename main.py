886cc9ca76e38237b7f232b074d4926ca9423895
unknown <alexboyuchen@gmail.com>
2021-05-21 01:11:27 -0700

    added a menu screen

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import math,random,time
+import math
+import random
+import time
 import neat
 import os
 import pygame
 from player import player
-from enemies import enemy,enemy2,enemy3
+from enemies import enemy, enemy2, enemy3
 
 pygame.init()
 
 winwidth = 1000
 winheight = 700
-window = pygame.display.set_mode((winwidth,winheight))
+window = pygame.display.set_mode((winwidth, winheight))
 difficulty = 4
 pygame.display.set_caption("Plane Game")
 run = True
+mainmenu = True
 training = True
 enemies = []
 wavecount = 0  # ten waves in each game
@@ -23,120 +26,137 @@ background = pygame.image.load("images/nightsky.png")
 background = pygame.transform.scale(background, (1000, 700))
 Cycle = 0
 Over = False  # if the game is over
-trainingmode = False # enter training mode (train neural network to play the game)
+# enter training mode (train neural network to play the game)
+trainingmode = False
 oldmove = 0   # avoid AI doing the same moves
 oldmove2 = 0
 showhitbox = False  # show enemy hitboxes (for debugging only)
-f = pygame.font.SysFont('comicsans',30,True)
-bigfont = pygame.font.SysFont('comicsans',80,True)
+f = pygame.font.SysFont('comicsans', 30, True)  # font
+mediumfont = pygame.font.SysFont('Comicsans', 50, True)
+bigfont = pygame.font.SysFont('comicsans', 80, True)
 players = []
 networks = []
 genes = []
 gennum = 0
+
+
 def determinecycle():  # determine how many cycles to the level
-    if difficulty <= 3: # dif 1-3 has 5 cycles
+    if difficulty <= 3:  # dif 1-3 has 5 cycles
         return 16
-    if difficulty <= 5 : # dif 4,5 has 6 cycles
+    if difficulty <= 5:  # dif 4,5 has 6 cycles
         return 17
     else:
         return 14  # dif 6 (insane mode) has 5 cycles
-def checkrands(target,rands): #helper function to spawnenemies()
-    if len(rands)!=0:
+
+
+def checkrands(target, rands):  # helper function to spawnenemies()
+    if len(rands) != 0:
         for rand in rands:
-            if abs(target-rand) <= 70 :  #  if distance between two enemies is within 70 then not ok
+            if abs(target-rand) <= 70:  # if distance between two enemies is within 70 then not ok
                 return False
     else:
         return True
     return True
+
+
 def computewavelength():
     if difficulty <= 2:
         return 4
     else:
         return 5
 
+
 def spawnenemies(Cycle):
     global enemies
     numenemy = (difficulty-3+(Cycle//2)) + wavecount
-    if numenemy <= 0 :  # numenemy can't be 0
+    if numenemy <= 0:  # numenemy can't be 0
         numenemy = 1
-    spawnbias = random.randint(Cycle//2,numenemy-1)
+    spawnbias = random.randint(Cycle//2, numenemy-1)
     numenemy = numenemy - spawnbias
     rands = []
     anticamp = []  # spawn enemies discourage camping
-    for i in range(0,numenemy):
+    for i in range(0, numenemy):
         ok = False
         failcounter = 0
         while not ok and failcounter <= 30:  # keep distance between enemies
-            x = random.randint(50,950)    # where on the x axis does the plane spawn
-            ok = checkrands(x,rands)
-            failcounter += 1        # if failed to generate enough distance too many times then go with it
+            # where on the x axis does the plane spawn
+            x = random.randint(50, 950)
+            ok = checkrands(x, rands)
+            # if failed to generate enough distance too many times then go with it
+            failcounter += 1
         rands.append(x)
     if difficulty >= 2 and trainingmode:
         anticamp.append(70)
         anticamp.append(817)
         for a in anticamp:
-            e = enemy2(a,0,window)
+            e = enemy2(a, 0, window)
             enemies.append(e)
     for rand in rands:
-        x = random.randint(0,difficulty-1)     # determine what enemy to generate
+        # determine what enemy to generate
+        x = random.randint(0, difficulty-1)
         if x == 0:
-            e = enemy(rand,0,window)
+            e = enemy(rand, 0, window)
         elif x == 1:
-            e = enemy2(rand,0,window)
+            e = enemy2(rand, 0, window)
         elif x == 2:
-            e = enemy3(rand,0,window)
+            e = enemy3(rand, 0, window)
         else:
-            e = enemy(rand,0,window)
+            e = enemy(rand, 0, window)
         enemies.append(e)
-        
 
-def checkplayer(player,enemies,Over):
+
+def checkplayer(player, enemies, Over):
     global pause
     global training
     global networks
     global genes
-    if player.dead and len(players)>0 and trainingmode:
+    if player.dead and len(players) > 0 and trainingmode:
         genes.pop(players.index(player))
-        networks.pop(players.index(player))   # remove the gene and network once its host dies
+        # remove the gene and network once its host dies
+        networks.pop(players.index(player))
         players.pop(players.index(player))
-        if len(players)==0: 
+        if len(players) == 0:
             training = False    # end generation
-    elif player.dead and len(players)==1 and not trainingmode:  
+    elif player.dead and len(players) == 1 and not trainingmode:
         pause = not pause
-        deathtext = bigfont.render('Game Over',1,(255,0,0) )
-        window.blit(deathtext, (330,250))
+        deathtext = bigfont.render('Game Over', 1, (255, 0, 0))
+        window.blit(deathtext, (330, 250))
 
     if Over and not player.dead and len(enemies) == 0:   # player won
         if trainingmode:
             training = False
-            return 
-        congrattext = bigfont.render('Congrats You Survived!!',1,(0,255,0) )
-        window.blit(congrattext, (130,250))
-        congrattext =  f.render('You Score:'+str(player.score),1,(69,123,255) )
-        window.blit(congrattext, (330,350))
+            return
+        congrattext = bigfont.render('Congrats You Survived!!', 1, (0, 255, 0))
+        window.blit(congrattext, (130, 250))
+        congrattext = f.render(
+            'You Score:'+str(player.score), 1, (69, 123, 255))
+        window.blit(congrattext, (330, 350))
         # enemies.clear()
 
-def observe(player): # observe the player's surroundings and find closest dangers
-    enemylist = [] # temp copy of list of enemies
+
+def observe(player):  # observe the player's surroundings and find closest dangers
+    enemylist = []  # temp copy of list of enemies
     for e in enemies:
         enemylist.append(e)
-    retlist = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    retlist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     counter = 2
-    for i in range (0,4):
+    for i in range(0, 4):
         dislist = []
         for e in enemylist:
             xdis = abs(e.x-player.xcoord)
             ydis = abs(e.y-player.ycoord)
             dis = math.sqrt(xdis**2+ydis**2)
-            dislist.append(dis) 
-        if len(dislist)!=0:     
-            mindis = min(dislist)   
-        else:       
+            dislist.append(dis)
+        if len(dislist) != 0:
+            mindis = min(dislist)
+        else:
             return retlist
-        index = dislist.index(mindis)                       
-        retlist[counter] = enemylist[index].hitboxes[0][0]           #append closest enemy's hitbox attributes to list
+        index = dislist.index(mindis)
+        # append closest enemy's hitbox attributes to list
+        retlist[counter] = enemylist[index].hitboxes[0][0]
         counter += 1
-        retlist[counter] = enemylist[index].hitboxes[0][2]  
+        retlist[counter] = enemylist[index].hitboxes[0][2]
         counter += 1
         retlist[counter] = enemylist[index].y   # also y position and enemy id
         counter += 1
@@ -147,7 +167,8 @@ def observe(player): # observe the player's surroundings and find closest danger
     retlist[1] = player.hitboxes[0][2]
     retlist[2] = player.hitboxes[0][1]
     return retlist
-        
+
+
 def redraw(keys):
     global wavecount
     global tickcount
@@ -157,9 +178,9 @@ def redraw(keys):
     global run
     global oldmove
     global oldmove2
-    window.blit(background,(0,0))
+    window.blit(background, (0, 0))
     if len(players)== 1 and not trainingmode:  # single player mode
-        healthtext = f.render('Health: '+ str(players[0].health),1,(0,255,0))   # display health,score and nukes
+        healthtext = f.render('Health: '+ str(players[0].health),0,(0,255,0))   # display health,score and nukes
         scoretext = f.render('Score: '+ str(players[0].score),1,(255,255,0))
         nuketext = f.render('Nukes: '+ str(players[0].nukes),1,(255,0,130))
         window.blit(healthtext,(800,0))
@@ -237,7 +258,7 @@ def redraw(keys):
 
 
 def eval_genomes(genomes,config):
-    #initialize everything everytime new generation gets run
+    # initialize everything everytime new generation gets run
     global pause
     global networks
     global genes
@@ -304,8 +325,25 @@ def train(config_file):
     # Run for up to 300 generations.
     winner = p.run(eval_genomes, 100)
 
+def main_menu():  # draws the main menu 
+    global mainmenu
+    global run,window
+    while mainmenu:
+        gameclock.tick(50)
+        mainmenutext= mediumfont.render('Press any key to continue',1,(127,69,216))
+        window.blit(mainmenutext,(250,320))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                mainmenu = False
+                time.sleep(0.5)  # small deplay so the key pressed won't trigger any player action
+        pygame.display.update()
+
+
+
 def main():
-    global run
+    global run   # true if game has started
     global pause
     p = player(500,600,100,100)
     players.append(p)
@@ -320,7 +358,9 @@ def main():
             time.sleep(0.5)
         if pause:
             continue
-
+        mainmenutext = f.render('Press any keys to continue',1,(255,255,0))
+        window.blit(mainmenutext,(400,400))
+        window.blit(background,(0,0))
         redraw(keys)
     pygame.quit()
 
@@ -330,4 +370,5 @@ if __name__ == '__main__':
     if trainingmode:
         train(config_path)
     else:
+        main_menu()
         main()
